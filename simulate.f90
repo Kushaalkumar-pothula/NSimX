56e030898fd0ff4f898aa33f2c31fae659fe5a5c
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-05-17 22:57:47 +0530

    Add files via upload

---
+++ simulate.f90
@@ -0,0 +1,150 @@
+subroutine acceleration(pos, mass, soft, acc)
+    !====================================================================!
+    !              Calculates accelerations of N particles               !
+    !             ------------------------------------------             !
+    ! Inputs:                                                            !
+    ! ------                                                             !
+    !    - pos: N x 3 matrix of positions                                !
+    !    - mass: N  1 matrix of masses                                   !
+    !    - soft: softening                                               !
+    !                                                                    !
+    ! Outputs:                                                           !
+    ! -------                                                            !
+    !    -acc: N x 3 matrix of accelerations                             !
+    !                                                                    !
+    !====================================================================!
+    implicit none
+    
+    !---Type Declarations-------------------------------------------------
+    real, allocatable, intent(in) :: pos(:,:), mass(:)
+    real, intent(in) :: soft
+    real, allocatable, intent(out) :: acc(:,:)
+    integer :: i,j
+    real :: dx, dy, dz, inv_r3
+
+    real :: G
+    integer :: N
+    !--------------------------------------------------------------------
+
+    !---Initialization---------------------------------------------------
+    G = 6.673e-11   ! Gravitational Constant
+    N = size(pos,1) ! Numer of particles
+    allocate(acc(N,3))
+    acc(:,:) = 0
+    !--------------------------------------------------------------------
+
+    !---Main loop--------------------------------------------------------
+    do i = 1,N
+        do j = 1, N
+            dx = pos(j,1) - pos(i,1)
+            dy = pos(j,2) - pos(i,2)
+            dz = pos(j,3) - pos(i,3)
+
+            inv_r3 = (dx**2 + dy**2 + dz**2 + soft**2)**(-1.5)
+
+            acc(i,1) = acc(i,1) +  G * (dx * inv_r3) * mass(j)
+            acc(i,2) = acc(i,2) +  G * (dy * inv_r3) * mass(j)
+            acc(i,3) = acc(i,3) +  G * (dz * inv_r3) * mass(j)
+        end do
+    end do
+    !--------------------------------------------------------------------
+    
+end subroutine acceleration
+
+
+
+program simulate
+
+    implicit none
+    
+    !---Create interface to subroutine acceleration----------------------
+    interface
+        subroutine acceleration(pos, mass, soft, acc)
+        implicit none
+        real, allocatable,intent(in) :: pos(:,:)
+        real, allocatable, intent(in) :: mass(:)
+        real, intent(in) :: soft
+        real, allocatable, intent(out) :: acc(:,:)
+        end subroutine acceleration
+    end interface
+    !--------------------------------------------------------------------
+
+    !---Declarations---------------------------------------------------------------
+    real, allocatable :: ones(:)
+    real, allocatable :: mass(:)
+    real, allocatable :: pos(:,:)
+    real, allocatable :: vel(:,:)
+    real, allocatable :: pos_save(:,:,:)
+    real, allocatable :: acc(:,:)
+
+    real :: s, t, tEnd, dt
+
+    integer :: N, seed, Nt, i
+
+    N = 100     ! 100 particles
+    t = 0   ! Current tme of simulation
+    tEnd = 10.0    ! Time at which simulation ends
+    dt = 0.01    ! Timestep
+    s = 0.1     ! Softening
+    !------------------------------------------------------------------------------
+
+
+
+    !---Initial Conditions---------------------------------------------------------
+    seed = 17
+    call random_seed(size = seed)    ! Set random generator seed
+    
+    allocate(ones(N))
+    ones(:) = 1
+    
+    allocate(mass(N))
+    mass = (20.0*ones)/N
+    
+    ! Generate random positions
+    allocate(pos(N,3))
+    call random_number(pos)
+    
+    ! Generate random velocities
+    allocate(vel(N,3))
+    call random_number(vel)
+    
+    ! Calculate acceleration
+    allocate(acc(N,3))
+    call acceleration(pos, mass, s, acc)
+    
+    Nt = ceiling(tEnd/dt)   ! Time Step
+    
+    allocate(pos_save(N,3,Nt+1))
+    pos_save(:,:,1) = pos
+    !------------------------------------------------------------------------------
+    
+    
+    !---Main simulation loop-------------------------------------------------------
+    do i = 1, Nt
+    
+        vel = vel + ((acc*(dt/2.0)))  ! 1/2 Kick
+        pos = pos + (vel*dt)    ! Drift
+        
+        call acceleration(pos,mass,s, acc)  
+        
+        vel = vel + (acc*(dt/2.0))  ! 1/2 Kick
+        t = t + dt  ! Update time
+        
+        pos_save(:,:,i+1) = pos    ! Save positions
+        
+    end do
+    !------------------------------------------------------------------------------
+
+    !---Save Output----------------------------------------------------------------
+
+    open(10, file="pos_save.txt", status="new", action="write")
+    write(10, *) pos_save
+    close(10)
+    
+    open(12, file = "positions.txt", status = "new", action = "write")
+    write(12, *) pos
+    close(12)
+    !------------------------------------------------------------------------------
+    
+    
+end program simulate
